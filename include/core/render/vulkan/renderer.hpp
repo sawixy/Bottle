bcc4e02a58504ee9def2b878726c338beae31f8e
sawixy <sawixyy@gmail.com>
2026-03-29 21:22:04 +0300

    Renderer

--- include/core/render/vulkan/renderer.hpp
+++ include/core/render/vulkan/renderer.hpp
@@ -1,10 +1,39 @@
 #pragma once
 
+#include "vulkan/vulkan.hpp"
+#include <core/render/vulkan/queuemanager.hpp>
+#include <core/render/vulkan/context.hpp>
+#include <core/render/renderSystem.hpp>
+#include <vulkan/vulkan_raii.hpp>
+
 namespace bottle::core::render::vulkan {
 
 class VulkanRenderer {
 private:
-    
+    Context ctx{};
+    QueueManager queueManager{ctx};
+
+    // TODO: Collect Swapchain to a class
+    vk::raii::SwapchainKHR swapchain{nullptr};
+    std::vector<vk::raii::Image> images;
+    std::vector<vk::raii::ImageView> imageViews;
+
+    vk::PresentModeKHR presentMode;
+    vk::Extent2D rect;
+    vk::Format format;
+
+    std::vector<vk::raii::Semaphore> renderReady;
+    std::vector<vk::raii::Semaphore> presentReady;
+    vk::raii::Fence fence{nullptr};
+
+public:
+    VulkanRenderer() {}
+
+    Context& getContext() { return ctx; }
+
+    void addMesh(Mesh mesh);
+
+    void render();
 };
 
 }
